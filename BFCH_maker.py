8fd4a0ea2bad42c6289aa78cdc719a40cb990995
DanilKrivonos <53526550+DanilKrivonos@users.noreply.github.com>
2019-12-12 00:15:11 +0300

    Update BFCH_maker.py

--- BFCH_maker.py
+++ BFCH_maker.py
@@ -7,8 +7,12 @@ parser.add_argument('-O_prod', type=float, help='O_prod', required=True)
 parser.add_argument('-C_start', type=int, help='C_start', required=True)
 parser.add_argument('-H_start', type=int, help='H_start', required=True)
 parser.add_argument('-O_start', type=int, help='O_start', required=True)
+parser.add_argument('-C_alt', type=int, help='C_alt', required=True)
+parser.add_argument('-H_alt', type=int, help='H_alt', required=True)
+parser.add_argument('-O_alt', type=int, help='O_alt', required=True)
+parser.add_argument('-G_C_alt_source', type=float, help='G_C_alt_source', required=True)
 parser.add_argument('-Y', type=float, help='ECONOM_COEF', required=True)
-parser.add_argument('-M_of_C_source', type=int, help='M(C_source)', required=True)
+parser.add_argument('-G_C_source', type=float, help='Gibbs energy of C source', required=True)
 args = parser.parse_args()
 
 A1 = 0
@@ -31,9 +35,9 @@ N_ = args.N_prod
 O_ = args.O_prod
 
 Mol_w_ADB = 12 * C_ + 1 * H_ + 14 * N_ + 16 * O_
-M_C = args.M_of_C_source
-print('M(ADB)', '=', Mol_w_ADB, 'g/mol')
-print('M', '(','C', C, 'H', H, 'O', O, ')', ' = ', M_C, 'g/mol')
+M_C = 12 * C + 1 * H + 16 * O
+print('M(ADB) ={} g/mol'.format(Mol_w_ADB))
+print('M(C{}H{}O{}) = {} g/mol'.format(C, H, O, M_C))
 #balance of C
 A5 = 1/Mol_w_ADB
 A1 = A5 * C_/C 
@@ -61,14 +65,14 @@ else:
 
 if A3 == 0:
     if A4 == 0:
-        print('Anabolism : ',  round(A1, 5), 'C', C, 'H', H, 'O', O, '+',  round(A2, 5), 'NH3OH', ' = ',  round(A5, 5), 'C', C_, 'H', H_, 'N', N_, 'O', O_, '+',  round(A6, 5), 'H20', '+', round(A7, 5), 'O2')
+        print('Anabolism : {} C{}H{}O{} + {} NH4OH  = {} C{}H{}N{}O{} + {} H20 + {} O2'.format(round(A1, 5), C, H, O, round(A2, 5), round(A5, 5), C_, H_, N_, O_, round(A6, 5), round(A7, 5)))
     else:
-        print('Anabolism : ', round(A1, 5), 'C', C, 'H', H, 'O', O, '+', round(A2, 5), 'NH3OH', '+', round(A4, 5), 'H2O', ' = ', round(A5, 5), 'C', C_, 'H', H_, 'N', N_, 'O', O_, '+', round(A7, 5), 'O2')
+        print('Anabolism : {} C{}H{}O{} + {} NH4OH + {} H2O  = {} C{}H{}N{}O{} + {} O2'.format(round(A1, 5), C, H, O, round(A2, 5), round(A4, 5), round(A5, 5), C_, H_, N_, O_, round(A7, 5)))
 else:
     if A4 == 0:
-        print('Anabolism : ', round(A1, 5), 'C', C, 'H', H, 'O', O, '+', round(A2, 5), 'NH3OH', '+', round(A3, 5), 'O2', ' = ', round(A5, 5), 'C', C_, 'H', H_, 'N', N_, 'O', O_, '+', round(A6, 5), 'H20')
+        print('Anabolism : {} C{}H{}O{} + {} NH4OH + {} O2 = {} C{}H{}N{}O{} +{} H20'.format(round(A1, 5), C, H, O, round(A2, 5), round(A3, 5), round(A5, 5), C_, H_, N_, O_, round(A6, 5)))
     else:
-        print('Anabolism : ', round(A1, 5), 'C', C, 'H', H, 'O', O, '+', round(A2, 5), 'NH3OH', '+', round(A3, 5), 'O2', '+', round(A4, 5), 'H2O', ' = ', round(A5, 5), 'C', C_, 'H', H_, 'N', N_, 'O', O_)
+        print('Anabolism : {} C{}H{}O{} + {} NH4OH + {} O2 +{} H2O = {} C{}H{}N{}O{}'.format(round(A1, 5), C, H, O, round(A2, 5), round(A3, 5), round(A4, 5), round(A5, 5), C_, H_, N_, O_, round(A6, 5)))
 
 #Catabolism
 K1 = 0
@@ -78,8 +82,8 @@ K4 = 0
 K5 = 0
 
 Y = args.Y
-S = 1 / Y
-print('ΔS', ' = ', S, 'g')
+S = round(1 / Y, 5)
+print('ΔS  = {} g'.format(S))
 #balance of C
 K1 = S / M_C - A1
 K3 = K1 * C
@@ -91,18 +95,151 @@ else:
     K2 = ((K3 * 2 + K4 * 1) - (K1 * O)) * 0.5
 
 if K2 == 0:
-    print('Catabolism : ', round(K1, 5), 'C', C, 'H', H, 'O', O, ' = ', round(K3, 5), 'CO2', '+', round(K4, 5), 'H2O', '+', round(K5, 5), 'O2' )
+    print('Catabolism : {} C{}H{}O{} = {} CO2 + {} H2O + {} O2'.format(round(K1, 5), C, H, O, round(K3, 5), round(K4, 5), round(K5, 5)))
 else:
-    print('Catabolism : ', round(K1, 5), 'C', C, 'H', H, 'O', O, '+', round(K2, 5), 'O2', ' = ', round(K3, 5), 'CO2', '+', round(K4, 5), 'H2O')
+    print('Catabolism : {} C{}H{}O{} + {} O2 = {} CO2 + {} H2O'.format(round(K1, 5), C, H, O, round(K2, 5), round(K3, 5), round(K4, 5)))
 
 #Metabolism
 if (round(A3, 5) + round(K2, 5)) > (round(A7, 5) + round(K5, 5)):
     if round(A4, 5) > (round(A6, 5) + round(K4, 5)):
-        print('Metabolism : ', round(round(A1, 5) + round(K1, 5), 5), 'C', C, 'H', H, 'O', O, '+',  round(A2, 5), 'NH3OH', '+', round((round(A3, 5) + round(K2, 5)) - (round(A7, 5) + round(K5, 5)), 5), 'O2', '+', round((round(A4, 5) - (round(A6, 5) + round(K4, 5))), 5), 'H2O', ' = ',  round(A5, 5), 'C', C_, 'H', H_, 'N', N_, 'O', O_, '+', round(K3, 5), 'CO2')
+        print('Metabolism : {} C{}H{}O{} + {} NH4OH + {} O2 + {} H2O = {} C{}H{}N{}O{} + {} CO2'.format(round(round(A1, 5) + round(K1, 5), 5), C, H, O, round(A2, 5), round((round(A3, 5) + round(K2, 5)) - (round(A7, 5) + round(K5, 5)), 5), round((round(A4, 5) - (round(A6, 5) + round(K4, 5))), 5), round(A5, 5), C_, H_, N_, O_, round(K3, 5)))
     else:
-        print('Metabolism : ', round(round(A1, 5) + round(K1, 5), 5), 'C', C, 'H', H, 'O', O, '+',  round(A2, 5), 'NH3OH', '+', round((round(A3, 5) + round(K2, 5)) - (round(A7, 5) + round(K5, 5)), 5), 'O2', ' = ',  round(A5, 5), 'C', C_, 'H', H_, 'N', N_, 'O', O_, '+', round((round(A6, 5) + round(K4, 5) - round(A4, 5)), 5), 'H20', '+', round(K3, 5), 'CO2')
+        print('Metabolism : {} C{}H{}O{} + {} NH4OH + {} O2 = {} C{}H{}N{}O{} + {} H20 + {} CO2'.format(round(round(A1, 5) + round(K1, 5), 5), C, H, O, round(A2, 5), round((round(A3, 5) + round(K2, 5)) - (round(A7, 5) + round(K5, 5)), 5), round(A5, 5), C_, H_, N_, O_, round((round(A6, 5) + round(K4, 5) - round(A4, 5)), 5), round(K3, 5)))
 else:
     if round(A4, 5) > (round(A6, 5) + round(K4, 5)):
-        print('Metabolism : ', round(round(A1, 5) + round(K1, 5), 5), 'C', C, 'H', H, 'O', O, '+',  round(A2, 5), 'NH3OH', '+', round((round(A3, 5) + round(K2, 5)), 5), 'O2', '+', round((round(A4, 5) - (round(A6, 5) + round(K4, 5))), 5), 'H2O', ' = ',  round(A5, 5), 'C', C_, 'H', H_, 'N', N_, 'O', O_, '+',  round(round(A6, 5) + round(K4, 5), 5), 'H20', '+', round(K3, 5), '+', 'CO2', round(((round(A7, 5) + round(K5, 5)) - (round(A3, 5) + round(K2, 5))), 5), 'O2')
+        print('Metabolism : {} C{}H{}O{} + {} NH4OH + {} H2O = {} C{}H{}N{}O{} + {} CO2 + {} O2'.format(round(round(A1, 5) + round(K1, 5), 5), C, H, O, round(A2, 5), round((round(A4, 5) - (round(A6, 5) + round(K4, 5))), 5), round(A5, 5), C_, H_, N_, O_, round(K3, 5), round(((round(A7, 5) + round(K5, 5)) - (round(A3, 5) + round(K2, 5))), 5)))
     else:
-        print('Metabolism : ', round(round(A1, 5) + round(K1, 5), 5), 'C', C, 'H', H, 'O', O, '+',  round(A2, 5), 'NH3OH', '+', round((round(A3, 5) + round(K2, 5)) - (round(A7, 5) + round(K5, 5)), 'O2', '+', round(A4, 5), 5), 'H2O', ' = ',  round(A5, 5), 'C', C_, 'H', H_, 'N', N_, 'O', O_, '+', round((round(A6, 5) + round(K4, 5) - round(A4, 5)), 5), 'H20', '+', round(K3, 5), 'CO2', round(((round(A7, 5) + round(K5, 5)) - (round(A3, 5) + round(K2, 5))), 5), 'O2')
+        print('Metabolism : {} C{}H{}O{} + {} NH4OH = {} C{}H{}N{}O{} + {} H20 + {} CO2 + {} O2'.format(round(round(A1, 5) + round(K1, 5), 5), C, H, O, round(A2, 5), round(A5, 5), round((round(A6, 5) + round(K4, 5) - round(A4, 5)), 5), round(K3, 5), round(((round(A7, 5) + round(K5, 5)) - (round(A3, 5) + round(K2, 5))), 5)))
+
+ΔG1 = args.G_C_source#C source
+#ignore ΔG2 NH4OH 
+ΔG3 = 0 #O2
+ΔG4 = -237.23#H2O
+#ΔG5 of ADB is not exist
+ΔG6 = -237.23#H2O
+ΔG7 = -394.37#CO2
+ΔG8 = 0#O2
+
+ΔG_of_reaction =0
+if (round(A3, 5) + round(K2, 5)) > (round(A7, 5) + round(K5, 5)):
+    if round(A4, 5) > (round(A6, 5) + round(K4, 5)):
+        ΔG_of_reaction = round(round(K3, 5) * ΔG7 - round((round(A4, 5) - (round(A6, 5) + round(K4, 5))), 5) * ΔG4 - round((round(A3, 5) + round(K2, 5)) - (round(A7, 5) + round(K5, 5)), 5) * ΔG3 - round(round(A1, 5) + round(K1, 5), 5) * ΔG1, 5)
+        print('ΔG(reaction) = ΔG(ADB) + ΔGdissipation + {} kJ/mol'.format(ΔG_of_reaction))
+    else:
+        ΔG_of_reaction = round(round(K3, 5) * ΔG7 + round((round(A6, 5) + round(K4, 5) - round(A4, 5)), 5) * ΔG6 - round((round(A3, 5) + round(K2, 5)) - (round(A7, 5) + round(K5, 5)), 5) * ΔG3 - round(round(A1, 5) + round(K1, 5), 5) * ΔG1, 5)
+        print('ΔG(reaction) = ΔG(ADB) + ΔGdissipation + {} kJ/mol'.format(ΔG_of_reaction))
+else:
+    if round(A4, 5) > (round(A6, 5) + round(K4, 5)):
+        ΔG_of_reaction = round(round(K3, 5) * ΔG7 + round(((round(A7, 5) + round(K5, 5)) - (round(A3, 5) + round(K2, 5))), 5) * ΔG8 - round((round(A4, 5) - (round(A6, 5) + round(K4, 5))), 5) * ΔG4 - round(round(A1, 5) + round(K1, 5), 5) * ΔG1, 5)
+        print('ΔG(reaction) = ΔG(ADB) + ΔGdissipation + {} kJ/mol'.format(ΔG_of_reaction))
+    else:
+        ΔG_of_reaction = round(round(K3, 5) * ΔG7 + round((round(A6, 5) + round(K4, 5) - round(A4, 5)), 5) * ΔG6 + round(((round(A7, 5) + round(K5, 5)) - (round(A3, 5) + round(K2, 5))), 5) * ΔG8 - round(round(A1, 5) + round(K1, 5), 5) * ΔG1, 5)
+        print('ΔG(reaction) = ΔG(ADB) + ΔGdissipation + {} kJ/mol'.format(ΔG_of_reaction))
+
+#alternative source of C
+C = args.C_alt
+H = args.H_alt
+O = args.O_alt
+#balance of C
+A5 = 1/Mol_w_ADB
+A1 = A5 * C_/C 
+#default NH4OH = NH5O
+#balance of H and O
+A2 = A5 * N_
+if (A1 * H + A2 * 5) > (A5 * H_):
+    A4 = 0 
+    A6 = ((A1 * H + A2 * 5) - (A5 * H_)) * 0.5
+    if (A1 * O + A2 * 1) > (A5 * O_ + A6 * 1):
+        A3 = 0 
+        A7 = ((A1 * O + A2 * 1) - (A5 * O_ + A6 * 1)) * 0.5
+    else:
+        A7 = 0
+        A3 = ((A5 * O_ + A6 * 1) - (A1 * O + A2 * 1)) * 0.5
+else:
+    A6 = 0
+    A4 = (A5 * H_ - A1 * H + A2 * 5) * 0.5
+    if (A1 * O + A2 * 1 + A4 * 1) > A5 * O_:
+        A3 = 0 
+        A7 = ((A1 * O + A2 * 1 + A4 * 1) - A5 * O_) * 0.5
+    else:
+        A7 = 0
+        A3 = (A5 * O_ - (A1 * O + A2 * 1 + A4 * 1)) * 0.5
+
+if A3 == 0:
+    if A4 == 0:
+        print('Anabolism of C{}H{}O{} : {} C{}H{}O{} + {} NH4OH  = {} C{}H{}N{}O{} + {} H20 + {} O2'.format(C, H, O, round(A1, 5), C, H, O, round(A2, 5), round(A5, 5), C_, H_, N_, O_, round(A6, 5), round(A7, 5)))
+    else:
+        print('Anabolism of C{}H{}O{} : {} C{}H{}O{} + {} NH4OH + {} H2O  = {} C{}H{}N{}O{} + {} O2'.format(C, H, O, round(A1, 5), C, H, O, round(A2, 5), round(A4, 5), round(A5, 5), C_, H_, N_, O_, round(A7, 5)))
+else:
+    if A4 == 0:
+        print('Anabolism of C{}H{}O{} : {} C{}H{}O{} + {} NH4OH + {} O2 = {} C{}H{}N{}O{} +{} H20'.format(C, H, O, round(A1, 5), C, H, O, round(A2, 5), round(A3, 5), round(A5, 5), C_, H_, N_, O_, round(A6, 5)))
+    else:
+        print('Anabolism of C{}H{}O{} : {} C{}H{}O{} + {} NH4OH + {} O2 +{} H2O = {} C{}H{}N{}O{}'.format(C, H, O, round(A1, 5), C, H, O, round(A2, 5), round(A3, 5), round(A4, 5), round(A5, 5), C_, H_, N_, O_, round(A6, 5)))
+#Catabolism
+K1 = 1
+K3 = K1 * C
+K4 = K1 * H * 0.5
+if K1 > (K3 * 2 + K4 * 1):
+    K5 = K1 * O - (K3 * 2 + K4 * 1) /2
+else:
+    K2 = ((K3 * 2 + K4 * 1) - K1 * O) /2
+
+if K1 > (K3 * 2 + K4 * 1):
+    print('Catabolism of C{}H{}O{} : {}X C{}H{}O{} = {}X CO2 + {}X H2O + {}X O2'.format(C, H, O, int(K1), C, H, O, int(K3), int(K4), K5))
+else:
+    print('Catabolism of C{}H{}O{} : {}X C{}H{}O{} + {}X O2 = {}X CO2 + {}X H2O'.format(C, H, O, int(K1), C, H, O, K2, int(K3), int(K4)))
+
+#Metabolism 
+if (A3 + K2) > (A7 + K5):
+    if A4 > (A6 + K4):
+        print('Metabolism of C{}H{}O{} : {}X+{} C{}H{}O{} + {} NH4OH + {}X+{} O2 + {}-{}X H2O = {} C{}H{}N{}O{} + {}X CO2'.format(C, H, O, K1, round(A1 , 5), C, H, O, round(A2, 5), K2, round(A3, 5), round(A4, 5), K4, round(A5, 5), C_, H_, N_, O_, K3))
+    else:
+        print('Metabolism of C{}H{}O{} : {}X+{} C{}H{}O{} + {} NH4OH + {}X+{} O2 = {} C{}H{}N{}O{} + {}X CO2 + {}X+{} H2O'.format(C, H, O, K1, round(A1 , 5), C, H, O, round(A2, 5), K2, round(A3, 5), round(A5, 5), C_, H_, N_, O_, K3, K4, round(A6, 5)))
+else:
+    if A4 > (A6 + K4):
+        print('Metabolism of C{}H{}O{} : {}X+{} C{}H{}O{} + {} NH4OH + {}-{}X H2O = {} C{}H{}N{}O{} + {}X CO2 + {}X+{} O2'.format(C, H, O, K1, round(A1 , 5), C, H, O, round(A2, 5), round(A4, 5), K4, round(A5, 5), C_, H_, N_, O_, K3, K5, round(A7, 5)))
+    else:
+        print('Metabolism of C{}H{}O{} : {}X+{} C{}H{}O{} + {} NH4OH = {} C{}H{}N{}O{} + {}X CO2 + {}X+{} H2O + {}X-{} O2'.format(C, H, O, K1, round(A1 , 5), C, H, O, round(A2, 5), C, H, O,round(A5, 5), C_, H_, N_, O_, K3, K4, round(A6, 5), K5, round(A7, 5)))
+  
+ΔG1 = args.G_C_alt_source#C source
+
+ΔG_of_reaction_alt_C = 0
+ΔG_of_reaction_alt_CX = 0
+if (round(A3, 5) + round(K2, 5)) > (round(A7, 5) + round(K5, 5)):
+    if round(A4, 5) > (round(A6, 5) + round(K4, 5)):
+        ΔG_of_reaction_alt_CX = K3 * ΔG7 + K2 * ΔG4 - K4 * ΔG3 - K1 * ΔG1
+        ΔG_of_reaction_alt_C = - round(A4, 5) * ΔG4 -  round(A3, 5) * ΔG3 - round(A1 , 5) * ΔG1
+        print('ΔG(reaction with alternative C) = ΔG(ADB) + ΔGdissipation + {}X + {} kJ/mol'.format(ΔG_of_reaction_alt_CX, ΔG_of_reaction_alt_C))
+    else:
+        ΔG_of_reaction_alt_CX = K3 * ΔG7 + K4 * ΔG6 - K2 * ΔG3 - K1 * ΔG1
+        ΔG_of_reaction_alt_C = round(A6, 5) * ΔG6 + round(A3, 5) * ΔG3 - round(A1 , 5) * ΔG1
+        print('ΔG(reaction with alternative C) = ΔG(ADB) + ΔGdissipation + {}X + {} kJ/mol'.format(ΔG_of_reaction_alt_CX, ΔG_of_reaction_alt_C))
+else:
+    if round(A4, 5) > (round(A6, 5) + round(K4, 5)):
+        ΔG_of_reaction_alt_CX = K5 * ΔG8 + K3 * ΔG7 + K4 * ΔG4 - K1 * ΔG1
+        ΔG_of_reaction_alt_C =  + round(A7, 5) * ΔG8 - round(A4, 5) *ΔG4 - round(A1 , 5) * ΔG1
+        print('ΔG(reaction with alternative C) = ΔG(ADB) + ΔGdissipation + {}X + {} kJ/mol'.format(ΔG_of_reaction_alt_CX, ΔG_of_reaction_alt_C))
+    else:
+        ΔG_of_reaction_alt_CX = K3 * ΔG7 + K5 * ΔG6 + K7 * ΔG8 - K1 * ΔG1
+        ΔG_of_reaction_alt_C = - round(A7, 5)* ΔG6 + round(A6, 5) * ΔG8 - round(A1, 5) * ΔG1
+        print('ΔG(reaction with alternative C) = ΔG(ADB) + ΔGdissipation + {}X + {} kJ/mol'.format(ΔG_of_reaction_alt_CX, ΔG_of_reaction_alt_C))
+
+x = round((ΔG_of_reaction - ΔG_of_reaction_alt_C) /ΔG_of_reaction_alt_CX, 5)
+
+#True Catabolism
+if K1 > (K3 * 2 + K4 * 1):
+    print('Catabolism of C{}H{}O{} : {} C{}H{}O{} = {} CO2 + {} H2O + {} O2'.format(C, H, O, round(int(K1) * x, 5), C, H, O, round(int(K3) * x, 5), round(int(K4) * x, 5), round(K5 * x, 5)))
+else:
+    print('Catabolism of C{}H{}O{} : {} C{}H{}O{} + {} O2 = {} CO2 + {} H2O'.format(C, H, O, round((int(K1) * x), 5), C, H, O, round(K2 * x, 5), round(int(K3) * x, 5), round(int(K4) * x, 5)))
+
+#True Metabolism
+if (A3 + K2) > (A7 + K5):
+    if A4 > (A6 + K4):
+        print('Metabolism of C{}H{}O{} : {} C{}H{}O{} + {} NH4OH + {} O2 + {} H2O = {} C{}H{}N{}O{} + {} CO2'.format(C, H, O, round(K1 * x + round(A1 , 5), 5), C, H, O, round(A2, 5), round(K2 * x + round(A3, 5), 5), round(round(A4, 5) - K4 * x, 5), round(A5, 5), C_, H_, N_, O_, round(K3 * x, 5)))
+    else:
+        print('Metabolism of C{}H{}O{} : {} C{}H{}O{} + {} NH4OH + {} O2 = {} C{}H{}N{}O{} + {} CO2 + {} H2O'.format(C, H, O, round(K1 * x + round(A1 , 5), 5), C, H, O, round(A2, 5), round(K2 * x + round(A3, 5), 5), round(A5, 5), C_, H_, N_, O_, round(K3 * x, 5), round(K4 * x + round(A6, 5), 5)))
+else:
+    if A4 > (A6 + K4):
+        print('Metabolism of C{}H{}O{} : {} C{}H{}O{} + {} NH4OH + {} H2O = {} C{}H{}N{}O{} + {} CO2 + {} O2'.format(C, H, O, round(K1 * x + round(A1 , 5), 5), C, H, O, round(A2, 5), round(round(A4, 5) - x * K4), round(A5, 5), C_, H_, N_, O_, round(K3 * x, 5), round(K5 * x + round(A7, 5), 5)))
+    else:
+        print('Metabolism of C{}H{}O{} : {} C{}H{}O{} + {} NH4OH = {} C{}H{}N{}O{} + {} CO2 + {} H2O + {} O2'.format(C, H, O, round(K1 * x + round(A1 , 5), 5), C, H, O, round(A2, 5), C, H, O,round(A5, 5), C_, H_, N_, O_, round(K3 * x, 5), round(K4 * x + round(A6, 5), 5), round(K5 * x - round(A7, 5), 5)))
